e4ade74cef1821a5b84c642b95e6ea0ace8b23f3
aarushkadam25-collab <aarushkadam25@gmail.com>
2025-09-28 10:49:28 +0200

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+print(" Welcome to the Square Root Finder App!")
+while True:
+        
+    num = float(input("Enter a number: "))
+    if num < 0:
+                print(" Sorry, cannot find the square root of a negative number.")
+    
+    
+    
+    else:
+                result = (num)
+                print("The square root of", num, "is", result)
+        
+           
+    print("👋 Goodbye!")
+    break
+
+
